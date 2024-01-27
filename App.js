80d8bb6231955b7d09b08fbb0dfaa5ef52c4ac48
ananas2109 <kweanhngo@gmail.com>
2024-01-27 01:06:45 -0500

    onesignal2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppContainer from './src/navigations/AppNavigation';
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 
 export default function App() {
   // Remove this method to stop OneSignal Debugging
